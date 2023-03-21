b6feb42a64536b33186f57c076c6f4de2e1e7e79
Nika-On <nikaonezashvili837@gmail.com>
2023-03-21 22:22:55 +0400

    implementing new features

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+//defining variables
 const number = document.querySelectorAll("button");
 const screen = document.querySelector(".display-tab");
+const iconDay = document.querySelector("#active");
+const iconNight = document.querySelector("#passive");
+const body = document.querySelector("body");
+const modeText = document.querySelector(".mode-text")
 let value;
 let result = "";
+//extracting values from buttons and calculating result after clicking "="
 number.forEach((element) => {
   element.addEventListener("click", () => {
     value = element.textContent;
@@ -9,10 +15,39 @@ number.forEach((element) => {
       result = eval(result);
       screen.textContent = result;
     } else if (value === "C") {
-        screen.textContent = "";
+      screen.textContent = "";
+      result = "";
     } else {
       result += value;
       screen.textContent = result;
     }
   });
 });
+//switching to daymode and vise versa
+iconDay.addEventListener("click",()=>{
+        iconDay.removeAttribute("id","active");
+        iconDay.setAttribute("id","passive");
+        iconNight.setAttribute("id","active");
+        body.style.backgroundColor = "#12D8FA";
+        modeText.textContent = "Switch to Night mode";
+        number.forEach(
+            (element)=>{
+                element.classList.add("DayMode")
+            }
+        );
+        screen.classList.add("DayMode")
+});
+iconNight.addEventListener("click",()=>{
+    iconNight.removeAttribute("id","active");
+    iconDay.setAttribute("id","active");
+    iconNight.setAttribute("id","passive")
+    body.style.backgroundColor = "#141313";
+    modeText.textContent = "Switch to Day mode";
+    number.forEach(
+        (element)=>{
+            element.classList.remove("DayMode")
+        }
+    );
+    screen.classList.remove("DayMode")
+});
+
